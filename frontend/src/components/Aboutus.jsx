1ddf01fd744e6e72436543f14d61549b71750d0f
roshni jaisinghani <roshnijaisinghani@Rahuls-MacBook-Pro.local>
2023-07-09 04:42:48 +0530

    added courses

--- frontend/src/components/Aboutus.jsx
+++ frontend/src/components/Aboutus.jsx
@@ -1,22 +1,4 @@
-// import React from 'react'
-// //import { Link } from 'react-router-dom'
 
-
-// function About() {
-//   return (
-//     <div class="card" >
-//   <img class="card-img-top" src="..." alt="Card image cap"/>
-//   <div class="card-body">
-//     <h5 class="card-title">Card title</h5>
-//     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-//     <a href="#" class="btn btn-primary">Go somewhere</a>
-//   </div>
-// </div>
-  
-//   )
-// }
-
-// export default About
 import React from 'react';
 
 
