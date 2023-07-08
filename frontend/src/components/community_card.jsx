6a54b1986b3aeb885741678390ba7e476a1a3951
roshni jaisinghani <roshnijaisinghani@Rahuls-MacBook-Pro.local>
2023-07-09 02:41:22 +0530

    updated aboutjs

--- frontend/src/components/community_card.jsx
+++ frontend/src/components/community_card.jsx
@@ -33,6 +33,25 @@ function community_card(props) {
           </div>
         </div>
       </div>
+      <div className=" col-sm-3 mb-4">
+        <div class="card shadow-lg" >
+          <img
+            src="https://prod-discovery.edx-cdn.org/media/course/image/52bf4539-6137-4968-9605-6c32414dcdc4-7e805a266b31.small.png"
+            class="card-img-top"
+            alt="..."
+          />
+          <div class="card-body">
+            <h5 class="card-title">Course title {props.sq}</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+      </div>
       
     </div>
     
